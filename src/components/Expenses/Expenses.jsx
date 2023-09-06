f184652cb683e7edf6bdb6d7105450caef0748c4
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-09-06 18:15:52 +0400

    expensesmodified

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,7 +2,18 @@ import "./Expenses.css";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 
 export default function Expense({ items }) {
-  return <div >
-    
-  </div>;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+    </div>
+  );
 }
