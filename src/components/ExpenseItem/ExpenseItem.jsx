cffb2cb977a78ecd0920cbb2c9991762dcbbe3cb
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-09-06 18:28:51 +0400

    expenseItemAndCard

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
 import ExpenseData from "../ExpenseData/ExpenseData";
+import Card from "../Card/Card";
 
 export default function ExpenseItem({ date, title, amount }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseData date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
