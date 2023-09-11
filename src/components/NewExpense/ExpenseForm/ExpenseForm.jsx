927e784584caf1101b9110bc8fa2d593cc90354b
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-09-11 10:43:29 +0400

    styles

--- src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
-  return <form></form>;
+  return (
+    <form>
+      <div className="new-expense_controls">
+        <div className="new-expense_control">
+          <label>Title</label>
+          <input type='text'/>
+        </div>
+      </div>
+    </form>
+  );
 }
