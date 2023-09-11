99817e518d69255501ef1b6a3a12f9d9039082bb
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-09-11 10:46:43 +0400

    date

--- src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
@@ -9,9 +9,15 @@ export default function ExpenseForm() {
           <label>Title</label>
           <input type="text" />
         </div>
+
         <div className="new-expense_control">
           <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
+        </div>
+
+        <div className="new-expense_control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2024-12-31" />
         </div>
       </div>
     </form>
